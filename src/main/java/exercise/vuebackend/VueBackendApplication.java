b692560ac2aa24a7e58e610b9c12c223c58ebab2
hmydt <woal136@naver.com>
2024-02-06 16:49:15 +0900

    Initial commit

---
+++ src/main/java/exercise/vuebackend/VueBackendApplication.java
@@ -0,0 +1,13 @@
+package exercise.vuebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exercise/vuebackend/VueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.vuebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
